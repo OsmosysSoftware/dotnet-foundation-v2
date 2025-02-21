338d4ab6eb97e1e4287b5dc1dc3fe7c4512eb692
Jatin Gupta <jatin.g@osmosys.co>
2025-02-21 15:42:57 +0530

    Add migrations for inital database setup

---
+++ src/Api/Migrations/20250220183440_InitialCreate.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations;
+
+/// <inheritdoc />
+public partial class InitialCreate : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterDatabase()
+            .Annotation("MySql:CharSet", "utf8mb4");
+
+        migrationBuilder.CreateTable(
+            name: "roles",
+            columns: table => new
+            {
+                id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                updated_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_roles", x => x.id);
+            })
+            .Annotation("MySql:CharSet", "utf8mb4");
+
+        migrationBuilder.CreateTable(
+            name: "users",
+            columns: table => new
+            {
+                id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                first_name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                last_name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                email = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                password_hash = table.Column<string>(type: "longtext", nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                role_id = table.Column<int>(type: "int", nullable: false),
+                created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                updated_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_users", x => x.id);
+                table.ForeignKey(
+                    name: "FK_users_roles_role_id",
+                    column: x => x.role_id,
+                    principalTable: "roles",
+                    principalColumn: "id",
+                    onDelete: ReferentialAction.Restrict);
+            })
+            .Annotation("MySql:CharSet", "utf8mb4");
+
+        migrationBuilder.CreateTable(
+            name: "user_logs",
+            columns: table => new
+            {
+                id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                action = table.Column<string>(type: "longtext", nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                user_id = table.Column<int>(type: "int", nullable: false),
+                created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                updated_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_user_logs", x => x.id);
+                table.ForeignKey(
+                    name: "FK_user_logs_users_user_id",
+                    column: x => x.user_id,
+                    principalTable: "users",
+                    principalColumn: "id",
+                    onDelete: ReferentialAction.Cascade);
+            })
+            .Annotation("MySql:CharSet", "utf8mb4");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_user_logs_user_id",
+            table: "user_logs",
+            column: "user_id");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_users_role_id",
+            table: "users",
+            column: "role_id");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "user_logs");
+
+        migrationBuilder.DropTable(
+            name: "users");
+
+        migrationBuilder.DropTable(
+            name: "roles");
+    }
+}
