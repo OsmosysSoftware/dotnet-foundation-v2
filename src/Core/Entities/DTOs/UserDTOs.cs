6bb537a3f2e24448ea874ae58caebf6163e60366
Jatin Gupta <jatin.g@osmosys.co>
2025-02-23 01:58:23 +0530

    Add User DTOs

---
+++ src/Core/Entities/DTOs/UserDTOs.cs
@@ -0,0 +1,26 @@
+namespace Core.Entities.DTOs;
+
+public class UserCreateDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public int RoleId { get; set; }
+}
+
+public class UserUpdateDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int RoleId { get; set; }
+}
+
+public class UserResponseDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}
