a796cda627d5a5a616c9af9552154f1e6829db2d
Jatin Gupta <jatin.g@osmosys.co>
2025-02-28 04:33:25 +0530

    Update the IRoleService

--- src/Core/Services/Interfaces/IRoleService.cs
+++ src/Core/Services/Interfaces/IRoleService.cs
@@ -7,7 +7,7 @@ public interface IRoleService
 {
     Task<RoleResponseDto?> GetRoleByIdAsync(int id);
     Task<IEnumerable<RoleResponseDto>> GetAllRolesAsync();
-    Task<RoleResponseDto> CreateRoleAsync(RoleCreateDto roleDto);
+    Task<RoleResponseDto?> CreateRoleAsync(RoleCreateDto roleDto);
     Task<bool> UpdateRoleAsync(int id, RoleUpdateDto roleDto);
     Task<bool> DeleteRoleAsync(int id);
 }
