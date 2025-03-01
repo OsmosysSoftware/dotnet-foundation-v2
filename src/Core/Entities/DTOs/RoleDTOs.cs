41ded7e7410fe891319f9353b937080ba0c7b0b2
Jatin Gupta <jatin.g@osmosys.co>
2025-03-01 10:32:53 +0530

    Updated the class name

--- src/Core/Entities/DTOs/RoleDTOs.cs
+++ src/Core/Entities/DTOs/RoleDTOs.cs
@@ -2,22 +2,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Core.Entities.DTOs;
 
-public class RoleDto
+public class RoleBaseDto
 {
     [Required(ErrorMessage = "Role name is required")]
     [StringLength(50, MinimumLength = 2, ErrorMessage = "Role name must be between 2 and 50 characters")]
     public string Name { get; set; } = string.Empty;
 }
 
-public class RoleCreateDto: RoleDto
+public class RoleCreateDto: RoleBaseDto
 {
 }
 
-public class RoleUpdateDto: RoleDto
+public class RoleUpdateDto: RoleBaseDto
 {
 }
 
-public class RoleResponseDto: RoleDto
+public class RoleResponseDto: RoleBaseDto
 {
     public int Id { get; set; }
 }
