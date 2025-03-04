8ce7858433ebce219392ca22564a43bb765f5846
Jatin Gupta <jatin.g@osmosys.co>
2025-03-04 11:25:01 +0530

    Create DTO for Login Request

--- src/Core/Entities/DTOs/UserDTOs.cs
+++ src/Core/Entities/DTOs/UserDTOs.cs
@@ -33,3 +33,10 @@ public class UserResponseDto
     public string Email { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
 }
+
+public class LoginRequestDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
