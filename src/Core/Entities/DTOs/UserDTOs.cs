c578b10eb53bf494d939509d3fa7511406004023
Jatin Gupta <jatin.g@osmosys.co>
2025-03-04 11:46:08 +0530

    Add validation attributes to LoginRequestDto

--- src/Core/Entities/DTOs/UserDTOs.cs
+++ src/Core/Entities/DTOs/UserDTOs.cs
@@ -36,7 +36,10 @@ public class UserResponseDto
 
 public class LoginRequestDto
 {
+    [Required, EmailAddress, MaxLength(255)]    
     public string Email { get; set; } = string.Empty;
+    
+    [Required]
     public string Password { get; set; } = string.Empty;
 }
 
